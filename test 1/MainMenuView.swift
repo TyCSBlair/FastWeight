995b0e2ee6a7bce616fc93c184338a27acb33dcb
Ty <tyc.s.blair@gmail.com>
2025-03-27 11:13:31 -0500

    Added dynamic domain for weight chart

--- test 1/MainMenuView.swift
+++ test 1/MainMenuView.swift
@@ -38,13 +38,34 @@ struct MainMenuView: View {
 struct weightChart: View {
     @Query private var weights: [Weight]
     var body: some View {
+        
         Chart(weights.sorted(by:{$0.datetaken < $1.datetaken})){
             LineMark(
                 x: .value("Date", $0.datetaken),
                 y: .value("Weight", $0.weightvalue)
             ).foregroundStyle(.green)
+        }.chartYScale(domain: [findMinWeight(weights: weights) - 10, findMaxWeight(weights: weights) + 10])
+    }
+}
+
+func findMinWeight( weights: [Weight]) -> Int {
+    var minWeight: Int = 1000
+    for i in weights {
+        if i.weightvalue < minWeight {
+            minWeight = i.weightvalue
         }
     }
+    return minWeight
+}
+
+func findMaxWeight( weights: [Weight]) -> Int {
+    var maxWeight: Int = 0
+    for i in weights {
+        if i.weightvalue > maxWeight {
+            maxWeight = i.weightvalue
+        }
+    }
+    return maxWeight
 }
 
 #Preview {
